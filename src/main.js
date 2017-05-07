0e864ef851c1d3bf6840e65490d43d2f2980cb44
roslanelyashiv <roslane@shellanoo.com>
2017-05-07 19:21:47 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueFire from 'vuefire'
+import App from './App'
+import router from './router'
+import VueParticles from 'vue-particles'
+
+Vue.use(VueFire)
+Vue.use(VueParticles)
+Vue.config.productionTip = false
+import 'toastr/build/toastr.min.css'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
