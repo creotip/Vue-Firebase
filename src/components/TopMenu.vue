f120822a551b180f6fe64a590ff940fb00dc0dce
roslanelyashiv <roslane@shellanoo.com>
2017-05-09 15:48:27 +0300

    first commit

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -5,7 +5,7 @@
         <button type="button" class="collapsed navbar-toggle" data-toggle="collapse"
                 data-target="#bs-example-navbar-collapse-5" aria-expanded="false"><span class="sr-only">Toggle navigation</span>
           <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span></button>
-        <a href="#" class="navbar-brand">Brand</a></div>
+        <a href="#" class="navbar-brand">Firebase</a></div>
       <div v-if="profile.length" class="collapse navbar-collapse" id="bs-example-navbar-collapse-5"><p
         class="navbar-text navbar-right">
         Signed in as <a href="#" class="navbar-link">{{ profile }}</a></p></div>
